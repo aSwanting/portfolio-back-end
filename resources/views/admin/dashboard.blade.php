deb281ff3509e664a661ec6786d229dfe838c598
aSwanting <gab.damico@gmail.com>
2024-01-23 23:57:22 +0100

    add: dashboard view tables

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,26 +1,90 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+    <div class="container">
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('Dashboard') }}
+        </h2>
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="card mb-4">
+                    <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
                     </div>
-                    @endif
+                </div>
 
-                    {{ __('You are logged in!') }}
+                <div class="card mb-4">
+                    <div class="card-header">Database Items</div>
+                    <div class="card-body">
+                        <table class="table">
+                            <tbody>
+                                @forelse ($items as $item)
+                                    <tr>
+                                        <td>{{ $item->id }}</td>
+                                        <td><a href="{{ route('admin.items.show', $item) }}">{{ $item->name }}</a></td>
+                                        <td><a
+                                                href="{{ route('admin.categories.show', $item->category) }}">{{ $item->category->name }}</a>
+                                        </td>
+                                        {{-- <td>{{ $item->category->name }}</td> --}}
+                                    </tr>
+                                @empty
+                                @endforelse
+                                <tr></tr>
+                            </tbody>
+                        </table>
+                        {{ $items->links() }}
+                    </div>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-header">Database Categories</div>
+                    <div class="card-body">
+                        <table class="table">
+                            <tbody>
+                                @forelse ($categories as $category)
+                                    <tr>
+                                        <td>{{ $category->id }}</td>
+                                        <td><a
+                                                href="{{ route('admin.categories.show', $category) }}">{{ $category->name }}</a>
+                                        </td>
+                                    </tr>
+                                @empty
+                                @endforelse
+                                <tr></tr>
+                            </tbody>
+                        </table>
+                        {{ $categories->links() }}
+                    </div>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-header">Database Tags</div>
+                    <div class="card-body">
+                        <table class="table">
+                            <tbody>
+                                @forelse ($tags as $tag)
+                                    <tr>
+                                        <td>{{ $tag->id }}</td>
+                                        <td><a href="{{ route('admin.tags.show', $tag) }}">{{ $tag->name }}</a>
+                                        </td>
+                                    </tr>
+                                @empty
+                                @endforelse
+                                <tr></tr>
+                            </tbody>
+                        </table>
+                        {{ $tags->links() }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
