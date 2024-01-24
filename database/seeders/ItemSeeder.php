d9ebd3dd1a19fed5e230cc2e22d87bf18dc67a70
aSwanting <gab.damico@gmail.com>
2024-01-24 14:34:28 +0100

    removed faker from itemseeder

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Item;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 use App\Models\Category;
 use App\Models\Tag;
@@ -15,13 +14,13 @@ class ItemSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(Faker $faker): void
+    public function run(): void
     {
-        for ($i = 0; $i < 50; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $new_item = new Item();
-            $new_item->name = $faker->sentence(rand(2, 5));
+            $new_item->name = 'Item ' . $i + 1;
             $new_item->slug = Str::slug($new_item->name);
-            $new_item->description = $faker->text(250);
+            $new_item->description = '...';
             $new_item->category_id = Category::all()->random()->id;
             $new_item->save();
             $new_item->tags()->attach(Tag::all()->random(mt_rand(0, Tag::count())));
